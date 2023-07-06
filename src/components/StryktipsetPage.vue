05b7e168852a51bf5c18b4127a897ade9cc6af5c
wilhelmgroth <115974049+wilhelmgrothbl@users.noreply.github.com>
2023-07-06 15:14:53 +0200

    stfff

--- src/components/StryktipsetPage.vue
+++ src/components/StryktipsetPage.vue
@@ -1,10 +1,14 @@
 <template>
-  <div class="bg-cover bg-center text-gray-800 bg-gray-100 ">
+  <div class="bg-cover bg-center text-gray-800 bg-gray-100">
     <navbar></navbar>
     <div class="container mx-auto p-8">
       <div class="bg-white rounded-lg shadow-lg overflow-hidden m-4 p-8">
         <div class="flex items-center justify-center">
-          <img :src="require('@/assets/header.png')" alt="Stryktipset" class="w-7/8 h-1/3 object-cover mb-10 rounded-lg">  
+          <img
+            :src="require('@/assets/header.png')"
+            alt="Stryktipset"
+            class="w-7/8 h-1/3 object-cover mb-10 rounded-lg"
+          />
         </div>
 
         <div class="grid grid-cols-2 gap-8">
@@ -33,7 +37,7 @@
                   <th
                     class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
                   >
-                    Brutto
+                    Netto
                   </th>
                   <th
                     class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
@@ -107,7 +111,7 @@
             </table>
             <div class="mt-4">
               <h2 class="text-2xl font-semibold mb-4 text-blue-900">
-                Total insats och vinst/förlust
+                Total insats, vinst och brutto
               </h2>
               <table class="min-w-full divide-y divide-gray-900 shadow-sm">
                 <thead class="bg-gray-200">
@@ -120,15 +124,27 @@
                     <th
                       class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
                     >
-                      Total vinst/förlust
+                      Total vinst
+                    </th>
+                    <th
+                      class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                    Netto
                     </th>
                   </tr>
                 </thead>
                 <tbody class="bg-white divide-y divide-gray-200">
                   <tr>
-  <td class="px-6 py-4 whitespace-nowrap">{{ formattedTotalInsats }}</td>
-  <td class="px-6 py-4 whitespace-nowrap">{{ formattedTotalVinstForlust }}</td>
-</tr>
+                    <td class="px-6 py-4 whitespace-nowrap">
+                      {{ formattedTotalInsats }}
+                    </td>
+                    <td class="px-6 py-4 whitespace-nowrap">
+                      {{ formattedTotalVinstForlust }}
+                    </td>
+                    <td class="px-6 py-4 whitespace-nowrap">
+                      {{ brutto }}
+                    </td>
+                  </tr>
                 </tbody>
               </table>
             </div>
@@ -184,11 +200,15 @@ export default {
       );
     },
     formattedTotalInsats() {
-    return Math.abs(this.totalInsats).toLocaleString('sv-SE') + ' kr';
-  },
-  formattedTotalVinstForlust() {
-    return Math.abs(this.totalVinstForlust).toLocaleString('sv-SE') + ' kr';
-  },
+      return this.totalInsats.toLocaleString("sv-SE") + " kr";
+    },
+    formattedTotalVinstForlust() {
+      return Math.abs(this.totalVinstForlust).toLocaleString("sv-SE") + " kr";
+    },
+    brutto() {
+      const bruttoValue = this.totalVinstForlust + this.totalInsats; // notera att tecknet på insatsen redan är negativt
+      return bruttoValue.toLocaleString("sv-SE") + " kr";
+    },
   },
 };
 </script>
@@ -197,7 +217,8 @@ export default {
 table {
   font-size: 14px;
 }
-h1, h2 {
+h1,
+h2 {
   text-shadow: 1px 1px 1px rgba(0, 0, 0, 0.1);
 }
 th {
