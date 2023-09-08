1a060a9ca903c43ba48f8e86c9e6c61d55187cad
wilhelmgroth <115974049+wilhelmgrothbl@users.noreply.github.com>
2023-09-08 16:15:29 +0200

    stuff

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,19 +1,19 @@
 <template>
   <nav class="bg-gray-900">
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-      <div class="flex items-center justify-between h-16">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 p-4">
+      <div class="flex items-center justify-between h-24 p-4">
         <div class="flex items-center">
-          <div class="flex-shrink-0">
-  <router-link to="/" class="h-8 w-8">
-    <img class="h-8 w-8" src="../assets/logo.png" alt="Logo">
-  </router-link>
-</div>
-
+          <div class="flex-shrink-0 p-4">
+            <router-link to="/" class="h-8 w-8 ">
+              <img class="h-24 w-24" src="../assets/highlogo.png" alt="Logo">
+            </router-link>
+          </div>
+          
           <div class="hidden md:block">
             <div class="ml-10 flex items-baseline space-x-4">
+              <router-link to="/" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Hem</router-link>
               <router-link to="/stryktipset" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Stryktipset</router-link>
               <router-link to="/v75" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">V75</router-link>
-              <router-link to="/" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Hem</router-link>
             </div>
           </div>
         </div>
